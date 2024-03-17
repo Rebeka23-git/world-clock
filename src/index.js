ed275b9dc6b2aab34d66683429d700eb859ee7ae
Rebeka23-git <158557757+Rebeka23-git@users.noreply.github.com>
2024-03-17 10:21:27 +0100

    Added functionality for the select

--- src/index.js
+++ src/index.js
@@ -19,5 +19,26 @@ function updateSydenyTime() {
     "h:mm:ss:SSS [<small>]A[</small>]"
   );
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElemnet = document.querySelector("#cities");
+  citiesElemnet.innerHTML = `
+  <div class="city">
+  <div>
+  <h2>${cityName}</h2>
+  <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss:SSS")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>`;
+}
+
 updateSydenyTime();
 setInterval(updateSydenyTime, 1);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
